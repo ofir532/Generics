6623919bbe712c1667f2224eb18da534710d7096
Ofir Dahan <ofird@10bis.co.il>
2022-03-11 21:49:57 +0200

    Add project files.

---
+++ Generics/HeroesTable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class HeroesTable
+    {
+        private Knight[] knights;
+        public HeroesTable(int size)
+        {
+            knights = new Knight[size];
+        }
+        public void AddHeroes(Knight kngiht)
+        {
+
+        }
+    }
+}
---
+++ Generics/Knight.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class Knight
+    {
+        private int Id { get; set; }
+        private string Name { get; set; }
+        private int Age { get; set; }
+        private string HorseType { get; set; }
+
+    }
+}
